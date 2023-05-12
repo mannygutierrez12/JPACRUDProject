1744e69d8b2dc785baa87ea772e99e8054f3df58
mannygutierrez12 <iberiard@gmail.com>
2023-05-12 19:09:08 -0400

    Create Spring Boot MVC Project

---
+++ TVShows/src/test/java/com/skilldistillery/tvshows/TvShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tvshows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
